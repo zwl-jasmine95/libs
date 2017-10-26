8f88f9c3d6ba4e9fed03c7fe07375c3324b79c7f
曾文莉 <zengwenli95@gmail.com>
2017-10-26 15:34:19 +0800

    图片轮播连续滑动效果

--- carousel/README.md
+++ carousel/README.md
@@ -1,1 +1,3 @@
 # 轮播图
+
+[demo](https://zwl-jasmine95.github.io/libs/carousel/demo.html)
--- carousel/src/z-carousel.js
+++ carousel/src/z-carousel.js
@@ -8,44 +8,44 @@
  * blog:
  * Project: https://github.com/zwl-jasmine95/libs/tree/master/carousel
  */
-class zCarousel{
-    constructor(obj){
+class zCarousel {
+    constructor(obj) {
         this.width = obj.width || 500
         this.height = obj.height || 300
 
         this._box = document.querySelector('#silder-box')
         this._content = document.querySelector('#silder-box .silder-content')
         this._li = document.querySelectorAll('#silder-box .silder-item')
-        this.len = this._li.length;
+        this.len = this._li.length
     }
     /**
      * 设置轮播区域宽高
      * 设置所有li标签的宽度
      */
-    setBoxSize(){
-        this._box.style.height = this.height + 'px';
-        this._box.style.width = this.width + 'px';
+    setBoxSize() {
+        this._box.style.height = this.height + 'px'
+        this._box.style.width = this.width + 'px'
 
-        this._li.forEach(_i=>{
-            _i.style.width = this.width + 'px';
-        });
+        this._li.forEach(_i => {
+            _i.style.width = this.width + 'px'
+        })
     }
 
     /**
      * 根据li标签的数量动态生成相同数量的小圆点
      */
-    creatCircle(){
-        const _circle = document.createElement('div');
-        
-        _circle.setAttribute('class','silder-circle');
-        
-        for(let i=0;i<this.len;i++){
-            let _item = document.createElement('div');
-            i == 0 ? _item.setAttribute('class','circle-item circle-item-active')
-                : _item.setAttribute('class','circle-item');
-            _circle.appendChild(_item); 
+    creatCircle() {
+        const _circle = document.createElement('div')
+
+        _circle.setAttribute('class', 'silder-circle')
+
+        for (let i = 0; i < this.len; i++) {
+            let _item = document.createElement('div')
+            i == 0 ? _item.setAttribute('class', 'circle-item circle-item-active')
+                : _item.setAttribute('class', 'circle-item')
+            _circle.appendChild(_item)
         }
-        this._box.appendChild(_circle); 
+        this._box.appendChild(_circle)
     }
 
 
@@ -54,32 +54,66 @@ class zCarousel{
      * @param {string} direction 滑动方向
      *  'left':向左滑(默认),'right':向右滑 
      */
-    round(direction){
-        const dir = direction || 'left';
-        const max_width = (this.len - 1) * this.width;
-        if(dir == 'left'){
-            let left = Math.abs(this._content.offsetLeft);
-            if(left < max_width){
-                this._content.style.left = -left - this.width + 'px';
-            }else{
-                this._content.style.left = 0;
+    // round(direction){
+    //     const dir = direction || 'left'
+    //     const max_width = (this.len - 1) * this.width
+    //     console.log(max_width)
+    //     if(dir == 'left'){
+    //         let left = Math.abs(this._content.offsetLeft)
+    //         if(left < max_width){
+    //             this._content.style.left = -left - this.width + 'px'
+    //         }else{
+    //             this._content.style.left = 0
+    //         }
+    //     }
+    // }
+    round(direction) {
+        const dir = direction || 'left'
+        const max_width = (this.len - 1) * this.width
+        const width = this.width
+        const _content = this._content
+
+        let timer
+
+        if (dir == 'left') {
+            let left = Math.abs(_content.offsetLeft)
+            let num = 0;
+            function animate() {
+                if (num <= max_width) {
+                    _content.style.left = - num + 'px';
+                    num += 10;
+
+                    if (num % width == 0) {
+                        console.log(timer);
+                        cancelAnimationFrame(timer)
+                    }
+                } else {
+                    _content.style.left = 0;
+                    num = 0;
+
+                }
+
+                timer = requestAnimationFrame(animate);
+                console.log(timer);
             }
+            animate();
+
         }
-
     }
 
-    carousel(){
-        setInterval(()=>{
+    carousel() {
+        setInterval(() => {
             this.round()
-        },1000)
+        }, 1000)
     }
 
 
 
-    initCarousel(){
-        this.setBoxSize();
-        this.creatCircle();
-        this.carousel();
+    initCarousel() {
+        this.setBoxSize()
+        this.creatCircle()
+        // this.carousel()
+        this.round()
     }
 
 }
